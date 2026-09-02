28d4e4d7cf65cff986d5f049815673a7ceea3a95
Digaz <diogop374@gmail.com>
2026-09-02 20:37:04 +0100

    Auth deactivated for now, will be re-enabled later.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,52 +1,20 @@
-import { useEffect, useState } from 'react'
-import { ActivityIndicator, View } from 'react-native'
-import { Stack, router } from 'expo-router'
-import { supabase } from '../lib/supabase'
+import { Stack } from 'expo-router'
 
 export default function RootLayout() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    checkSession()
-  }, [])
-
-  async function checkSession() {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession()
-
-    if (session) {
-      router.replace('/(tabs)')
-    } else {
-      router.replace('/login')
-    }
-
-    setLoading(false)
-  }
-
-  if (loading) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <ActivityIndicator size="large" />
-      </View>
-    )
-  }
-
   return (
     <Stack
       screenOptions={{
         headerShown: false,
       }}
     >
+      <Stack.Screen name="index" />
+
       <Stack.Screen name="login" />
+
       <Stack.Screen name="register" />
+
       <Stack.Screen name="(tabs)" />
+
       <Stack.Screen
         name="story/[id]"
         options={{
